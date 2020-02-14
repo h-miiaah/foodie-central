ad71c5b020ff2d860bb87243f1bcd02fbfe86bee
h-miiaah <haim.miiaah5@gmail.com>
2020-02-14 00:09:15 -0500

    adjusted recipes controller routes

--- foodie-central/app/controllers/recepies_controller.rb
+++ foodie-central/app/controllers/recepies_controller.rb
@@ -1,5 +1,20 @@
 class RecipesController < ApplicationController
     
+    get '/recipes/new' do
+        erb :'/recipes/new'
+    end
+    
+    post '/recipes' do
+        recipe = Recipe.new(params)
+        if !recipe.title.empty? && !recipe.instructions.empty? && !recipe.cook_time.empty?
+            recipe.save
+            redirect '/recipes'
+        else
+            @error = "All fields must be filled out. Please try again."
+            erb :'/recipes/new' 
+        end
+    end
+
     get '/recipes' do
         @recipes = Recipe.all
         erb :'recipes/index'
@@ -8,6 +23,22 @@ class RecipesController < ApplicationController
     get '/recipes/:id' do
         @recipe = Recipe.find(params[:id])
         erb :'recipes/show'
+    end
+
+    get '/recipes/:id/edit' do
+        @recipe = Recipe.find(params[:id])
+        erb :'recipes/edit'
+    end
+
+    patch '/recipes/:id' do
+        @recipe = Recipe.find(params[:id])
+        if !params["recipe"]["title"].empty? && !params["recipe"]["instructions"].empty? && !params["recipe"]["cook_time"].empty?
+        @recipe.update(params["recipe"])
+        redirect "/recipes/#{@recipe.id}"
+        else
+            @error = "All fields must be filled out. Please try again."
+            erb :'/recipes/edit' 
+        end
     end
 
 end
