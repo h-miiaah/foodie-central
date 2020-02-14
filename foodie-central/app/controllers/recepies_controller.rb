11ab355737eae3ae43cdca0b6ca13883a9e0008f
h-miiaah <haim.miiaah5@gmail.com>
2020-02-13 21:41:47 -0500

    added recipes controller

---
+++ foodie-central/app/controllers/recepies_controller.rb
@@ -0,0 +1,13 @@
+class RecipesController < ApplicationController
+    
+    get '/recipes' do
+        @recipes = Recipe.all
+        erb :'recipes/index'
+    end
+
+    get '/recipes/:id' do
+        @recipe = Recipe.find(params[:id])
+        erb :'recipes/show'
+    end
+
+end
